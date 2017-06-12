f54cad577ad1ca7214fbcded6d27b1c02214c9c4
Adam Burton <adz21c@gmail.com>
2017-06-12 02:05:50 +0100

    Initial read store setup.

---
+++ Leagues.ReadStore/ReadStoreDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Leagues.ReadStore
+{
+    public class ReadStoreDbContext : DbContext
+    {
+        public ReadStoreDbContext() : base("Leagues.ReadStore")
+        { }
+    }
+}
