801360e0ae672d11dc8db77d469fd17813c02431
Adam Burton <adz21c@gmail.com>
2019-03-03 12:47:45 +0000

    More unit test coverage.

---
+++ Miles.GreenPipes.UnitTests/ContainerScope/ContainerScopeTests.cs
@@ -0,0 +1,51 @@
+﻿using FakeItEasy;
+using GreenPipes;
+using Microsoft.Practices.ServiceLocation;
+using Miles.GreenPipes.ContainerScope;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles.GreenPipes.UnitTests.ContainerScope
+{
+    [TestFixture]
+    public class ContainerScopeTests
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public async Task Given_ABus_When_XScopesConfigured_Then_XScopesPushed(int targetDepth)
+        {
+            int containerDepth = 0;
+
+            var containerStack = A.Fake<IContainerStack>();
+            A.CallTo(() => containerStack.PushScope(A<TestContext>._)).Invokes(() => containerDepth++);
+            A.CallTo(() => containerStack.PopScope()).Invokes(() => containerDepth--);
+
+            var containerStackFactory = A.Fake<IContainerStackFactory>();
+            A.CallTo(() => containerStackFactory.Create(A<TestContext>.Ignored)).Returns(containerStack);
+
+            var pipe = Pipe.New<TestContext>(pc =>
+                {
+                    for (var i = 0; i < targetDepth; ++i)
+                        pc.UseContainerScope(containerStackFactory);
+
+                    pc.UseInlineFilter((ctx, next) =>
+                    {
+                        containerDepth.ShouldBe(targetDepth);
+                        return next.Send(ctx);
+                    });
+                });
+
+            await pipe.Send(new TestContext());
+        }
+
+        public class TestContext : BasePipeContext, PipeContext
+        { }
+    }
+
+}
---
+++ Miles.GreenPipes.UnitTests/TransactionContext/TransactionContextTests.cs
@@ -0,0 +1,97 @@
+﻿using FakeItEasy;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Practices.ServiceLocation;
+using Miles.Persistence;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles.GreenPipes.UnitTests.TransactionContext
+{
+    [TestFixture]
+    public class TransactionContextTests
+    {
+        [Test]
+        public async Task Given_ABusWithTransactionContext_When_Successful_Then_TransactionContextCommitted()
+        {
+            // Arrange
+            var transaction = A.Fake<ITransaction>();
+            A.CallTo(() => transaction.CommitAsync()).Returns(Task.CompletedTask);
+
+            var transactionContext = A.Fake<ITransactionContext>();
+            A.CallTo(() => transactionContext.BeginAsync(A<IsolationLevel?>._)).Returns(transaction);
+
+            var serviceLocator = A.Fake<IServiceLocator>();
+            A.CallTo(() => serviceLocator.GetInstance<ITransactionContext>()).Returns(transactionContext);
+
+            // Act
+            var pipe = Pipe.New<TestContext>(pc =>
+            {
+                pc.UseInlineFilter((ctx, next) =>
+                {
+                    ctx.GetOrAddPayload(() => serviceLocator);
+                    return next.Send(ctx);
+                });
+
+                pc.UseTransactionContext();
+
+                pc.UseInlineFilter((ctx, next) =>
+                {
+                    return next.Send(ctx);
+                });
+            });
+
+            await pipe.Send(new TestContext());
+
+            // Assert
+            A.CallTo(() => transactionContext.BeginAsync(A<IsolationLevel?>._)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => transaction.CommitAsync()).MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public async Task Given_ABusWithTransactionContext_When_Failure_Then_TransactionContextRolledback()
+        {
+            // Arrange
+            var transaction = A.Fake<ITransaction>();
+            A.CallTo(() => transaction.RollbackAsync()).Returns(Task.CompletedTask);
+
+            var transactionContext = A.Fake<ITransactionContext>();
+            A.CallTo(() => transactionContext.BeginAsync(A<IsolationLevel?>._)).Returns(transaction);
+
+            var serviceLocator = A.Fake<IServiceLocator>();
+            A.CallTo(() => serviceLocator.GetInstance<ITransactionContext>()).Returns(transactionContext);
+
+            // Act
+            var pipe = Pipe.New<TestContext>(pc =>
+            {
+                pc.UseInlineFilter((ctx, next) =>
+                {
+                    ctx.GetOrAddPayload(() => serviceLocator);
+                    return next.Send(ctx);
+                });
+
+                pc.UseTransactionContext();
+
+                pc.UseInlineFilter((ctx, next) =>
+                {
+                    throw new Exception();
+                });
+            });
+
+            await pipe.Send(new TestContext()).ShouldThrowAsync(typeof(Exception));
+
+            // Assert
+            A.CallTo(() => transactionContext.BeginAsync(A<IsolationLevel?>._)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => transaction.RollbackAsync()).MustHaveHappenedOnceExactly();
+        }
+
+        public class TestContext : BasePipeContext, PipeContext
+        { }
+    }
+}
