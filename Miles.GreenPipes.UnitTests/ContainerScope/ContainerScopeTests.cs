a2392b356b88709c237ed81bf51c1d588229018a
Adam Burton <adz21c@gmail.com>
2019-03-26 21:54:38 +0000

    Act and Assert now testing expectations.

--- Miles.GreenPipes.UnitTests/ContainerScope/ContainerScopeTests.cs
+++ Miles.GreenPipes.UnitTests/ContainerScope/ContainerScopeTests.cs
@@ -20,31 +20,56 @@ namespace Miles.GreenPipes.UnitTests.ContainerScope
         [TestCase(3)]
         public async Task Given_ABus_When_XScopesConfigured_Then_XScopesPushed(int targetDepth)
         {
+            // Arrange
             int containerDepth = 0;
+            var resolveStack = new Stack<Resolved>();
 
             var containerStack = A.Fake<IContainerStack>();
-            A.CallTo(() => containerStack.PushScope(A<TestContext>._)).Invokes(() => containerDepth++);
-            A.CallTo(() => containerStack.PopScope()).Invokes(() => containerDepth--);
+            A.CallTo(() => containerStack.PushScope(A<ExistingContext>._)).Invokes(() =>
+            {
+                containerDepth++;
+                resolveStack.Push(new Resolved { Value = containerDepth });
+            });
+            A.CallTo(() => containerStack.PopScope()).Invokes(() =>
+            {
+                containerDepth--;
+                resolveStack.Pop();
+            });
+            A.CallTo(() => containerStack.GetInstance<Resolved>()).ReturnsLazily(() => resolveStack.Peek());
 
             var containerStackFactory = A.Fake<IContainerStackFactory>();
-            A.CallTo(() => containerStackFactory.Create(A<TestContext>.Ignored)).Returns(containerStack);
+            A.CallTo(() => containerStackFactory.Create(A<ExistingContext>.Ignored)).Returns(containerStack);
 
-            var pipe = Pipe.New<TestContext>(pc =>
+            // Act and Assert
+            var pipe = Pipe.New<ExistingContext>(pc =>
                 {
                     for (var i = 0; i < targetDepth; ++i)
                         pc.UseContainerScope(containerStackFactory);
 
                     pc.UseInlineFilter((ctx, next) =>
                     {
-                        containerDepth.ShouldBe(targetDepth);
+                        if (!ctx.TryGetPayload(out IServiceLocator locator))
+                            Assert.Fail("No service locator");
+
+                        var resolved = locator.GetInstance<Resolved>();
+                        Assert.AreEqual(targetDepth, resolved.Value);
+
                         return next.Send(ctx);
                     });
                 });
 
-            await pipe.Send(new TestContext());
+            await pipe.Send(new ExistingContextImp());
         }
 
-        public class TestContext : BasePipeContext, PipeContext
+        public class Resolved
+        {
+            public int Value { get; set; }
+        }
+
+        public class ExistingContextImp : BasePipeContext, ExistingContext
+        { }
+
+        public interface ExistingContext : PipeContext
         { }
     }
 
