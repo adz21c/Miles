a143185ded12ccb736c68d489101143494dcec0f
Adam Burton <adz21c@gmail.com>
2019-04-06 18:14:14 +0100

    Licensing and documentation

--- Miles.Unity/DependencyInjection/UnityContainerExtensions.cs
+++ Miles.Unity/DependencyInjection/UnityContainerExtensions.cs
@@ -1,15 +1,44 @@
-﻿using Microsoft.Practices.Unity;
+﻿/*
+ *     Copyright 2019 Adam Burton (adz21c@gmail.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.Practices.Unity;
 using Miles.DependencyInjection;
 
 namespace Miles.Unity.DependencyInjection
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class UnityContainerExtensions
     {
+        /// <summary>
+        /// Returns a <see cref="IContainerBuilder"/> for the provided <paramref name="container"/>.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <returns></returns>
         public static IContainerBuilder ToContainerBuilder(this IUnityContainer container)
         {
             return new UnityContainerWrapper(container);
         }
 
+        /// <summary>
+        /// Returns a <see cref="IContainer"/> for the provided <paramref name="container"/>.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <returns></returns>
         public static IContainer ToContainer(this IUnityContainer container)
         {
             return new UnityContainerWrapper(container);
