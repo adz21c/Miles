3f0fd53aaf797aabff45d996b080a08789f1ffe4
Adam Burton <adz21c@gmail.com>
2020-05-05 21:40:54 +0100

    More transaction context tests

---
+++ src/Miles.Tests/GreenPipes/TransactionContext/TransactionContextExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FakeItEasy;
+using GreenPipes;
+using Microsoft.Extensions.DependencyInjection;
+using Miles.GreenPipes.ServiceScope;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Miles.Tests.GreenPipes.TransactionContext
+{
+    [TestFixture]
+    public class TransactionContextExtensionsTests
+    {
+        [Test]
+        public void Given_PipeConfigurator_When_UseTransactionContext_Then_Specification()
+        {
+            var pipeConfigurator = A.Fake<IPipeConfigurator<PipeContext>>();
+            var config = A.Dummy<Action<ITransactionContextConfigurator>>();
+
+            pipeConfigurator.UseTransactionContext(config);
+
+            A.CallTo(() => config.Invoke(A<ITransactionContextConfigurator>._)).MustHaveHappenedOnceExactly();
+        }
+    }
+
+}
