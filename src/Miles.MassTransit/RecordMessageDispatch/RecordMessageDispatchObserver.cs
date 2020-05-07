00267802cf198bc5a52deb9a66e6355c3f8e9091
Adam Burton <adz21c@gmail.com>
2020-05-07 16:20:21 +0100

    Record Message Dispatch Observer code coverage

--- src/Miles.MassTransit/RecordMessageDispatch/RecordMessageDispatchObserver.cs
+++ src/Miles.MassTransit/RecordMessageDispatch/RecordMessageDispatchObserver.cs
@@ -1,28 +1,31 @@
 ﻿using MassTransit;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Miles.MassTransit.RecordMessageDispatch
 {
     class RecordMessageDispatchObserver : ISendObserver
     {
-        private readonly IDispatchRecorder dispatchRecorder;
+        private readonly IDispatchRecorder _dispatchRecorder;
 
         public RecordMessageDispatchObserver(IDispatchRecorder dispatchRecorder)
         {
-            this.dispatchRecorder = dispatchRecorder;
+            _dispatchRecorder = dispatchRecorder ?? throw new ArgumentNullException(nameof(dispatchRecorder));
         }
 
         public Task PostSend<T>(SendContext<T> context) where T : class
         {
-            return dispatchRecorder.RecordAsync(context);
+            return _dispatchRecorder.RecordAsync(context);
         }
 
+        [ExcludeFromCodeCoverage]
         public Task PreSend<T>(SendContext<T> context) where T : class
         {
             return Task.CompletedTask;
         }
 
+        [ExcludeFromCodeCoverage]
         public Task SendFault<T>(SendContext<T> context, Exception exception) where T : class
         {
             return Task.CompletedTask;
